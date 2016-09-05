da449b227abb757496b58b15d126c7968b6bd186
Michael <michaelhuytran@gmail.com>
2016-09-05 12:58:28 -0700

    [Feature] - New Auth/facebook auth

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -1,5 +1,7 @@
 var db = require('../database/db.js')
 var _ = require('underscore')
+var scrypt = require("scrypt");
+
 
 var userModel = module.exports
 
@@ -12,6 +14,32 @@ userModel.addUser = function(attr) {
 		})
 	})
 }
+
+userModel.methods.generateHash = function(password){
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+	return bcrypt.compareSync(password, this.local.password);
+};
+
+
+// userModel.methods.generateHash = function(password){
+// 	var scryptParameters = scrypt.paramsSync(0.1);
+// 	var key = new Buffer(password); 
+
+// 	return scrypt.kdfSync(key, scryptParameters);
+//   	console.log(result.toString("hex"));
+
+// };
+
+// userSchema.methods.validPassword = function(password) {
+// 	var scryptParameters = scrypt.paramsSync(0.1);
+// 	var key = new Buffer(this.local.password); 
+// 	var kdfResult = scrypt.kdfSync(key, scryptParameters); 
+
+// 	return scrypt.verifyKdfSync(password, kdfResult);
+// }
 
 userModel.findUserById = function(params) {
 	return db('Users').where({
--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ var userRouter = require('./router/userRouter.js');
 var dishRouter = require('./router/dishRouter.js');
 var searchGoogleRouter = require('./router/searchGooglePlacesRouter.js');
 
+
+
 app.use(cors());
 app.use(bodyParse.json({limit: '50mb'}));
 
@@ -18,6 +20,26 @@ app.use(express.static('./client'));
 app.get('*', function (request, response){
   response.sendFile(path.resolve('./client', 'index.html'));
 });
+
+var passport = require('passport');
+var flash = require('connect-flash');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+require('./config/passport')(passport);
+app.use(morgan('dev')); 
+app.use(cookieParser());
+
+//app.set('view engine', 'ejs');
+
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./router/authRouter.js')(app, passport)
+
 
 app.use('/api/google', searchGoogleRouter);
 app.use('/api/user', userRouter);
